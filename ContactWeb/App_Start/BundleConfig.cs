d44433370e3ce1a342eb2dd304ed3be227f8bf94
Stefan Ivovic <stefanivovic91@gmail.com>
2017-01-24 13:23:49 +0100

    content,scrips changed

--- ContactWeb/App_Start/BundleConfig.cs
+++ ContactWeb/App_Start/BundleConfig.cs
@@ -9,22 +9,24 @@ namespace ContactWeb
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/Lib/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/Lib/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                      "~/Scripts/Lib/jquery.dataTables.min.js",
+                      "~/Scripts/Lib/dataTables.bootstrap.min.js",
+                      "~/Scripts/Lib/dataTables.colReorder.min.js"));
+
+            
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/jquery.dataTables.min.css",
+                      "~/Content/jquery.dataTables_themeroller.css",
+                      "~/Content/dataTables.bootstrap.min.css",
+                      "~/Content/colReorder.bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
